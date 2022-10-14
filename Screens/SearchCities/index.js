989ee8047bca49ffa50e9fce40407a5e90515f5e
20020399 Đào Thu Hằng <78419055+Haha02012021@users.noreply.github.com>
2022-10-14 16:17:26 +0700

    initial commit

---
+++ Screens/SearchCities/index.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function SearchCities() {
+    return (
+        <View>
+            <Text>Search Cities Screen</Text>
+        </View>
+    )
+}
---
+++ Screens/Welcome/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
+
+export default function Welcome({ navigation }) {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 10000,
+      useNativeDriver: true,
+    }).start();
+
+    setTimeout(() => {
+      navigation.navigate("Main");
+    }, 1100);
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
+      <Text style={styles.content}>TODAY'S WEATHER</Text>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+});
